9c8b923f403a17254b24613c825a2e01ff38647b
Teo <8duchuynh@gmail.com>
2023-09-25 11:45:16 +0700

    add villa controller, display villa list

---
+++ CleanArchitecture_Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace CleanArchitecture_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+    }
+}
