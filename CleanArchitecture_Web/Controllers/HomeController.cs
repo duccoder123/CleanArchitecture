14cc7c0685b24e5e79b10879fb70232810925e9e
Teo <8duchuynh@gmail.com>
2023-10-01 00:17:40 +0700

    add spinner action

--- CleanArchitecture_Web/Controllers/HomeController.cs
+++ CleanArchitecture_Web/Controllers/HomeController.cs
@@ -42,6 +42,7 @@ namespace CleanArchitecture_Web.Controllers
 
         public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
         {
+            Thread.Sleep(2000);
             var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
             foreach(var villa in villaList)
             {
