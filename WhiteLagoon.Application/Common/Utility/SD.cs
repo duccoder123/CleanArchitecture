7d0cfe296768f9fc0d35cb7ea76a7d975f4ee68f
Teo <8duchuynh@gmail.com>
2023-10-01 15:32:21 +0700

    status booking model

--- WhiteLagoon.Application/Common/Utility/SD.cs
+++ WhiteLagoon.Application/Common/Utility/SD.cs
@@ -10,5 +10,12 @@ namespace WhiteLagoon.Application.Common.Utility
     {
         public const string Role_Customer = "Customer";
         public const string Role_Admin  = "Admin";
+
+        public const string StatusPending = "Pending"; // chưa giải quyết
+        public const string StatusApproved = "Approved"; // xác nhận
+        public const string StatusCheckIn = "CheckedIn"; // đi vào
+        public const string StatusCompleted = "Completed"; // hoàn thành
+        public const string StatusCancelled = "Cancelled"; // huy bo
+        public const string StatusRefunded = "Refunded"; // đền bù 
     }
 }
